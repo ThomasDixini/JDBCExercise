e57373511ee86e678130122fb320a4413157f941
Thomás Dixini <thomasdixini@gmail.com>
2024-03-25 22:31:40 -0300

    :sparkles: Criando primeira conexão com banco de dados

--- src/main/java/thomasdixini/App.java
+++ src/main/java/thomasdixini/App.java
@@ -1,13 +1,37 @@
 package thomasdixini;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con;
+
+            System.out.println("Entrou");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "1234");
+            Statement stmt = con.createStatement();
+
+            String query = "create table SUPPLIERS" + "(SUP_ID integer NOT NULL, PRIMARY KEY(SUP_ID))";
+            stmt.executeUpdate(query);
+
+            ResultSet rs = stmt.executeQuery("select * from SUPPLIERS");
+            while (rs.next()) {
+                System.out.println(rs.getString("SUP_NAME"));
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.printf("Class Not Found" + e.getMessage());
+        } catch (SQLException ex) {
+            System.out.printf("Error connecting: %s", ex.getMessage());
+        }
     }
 }
