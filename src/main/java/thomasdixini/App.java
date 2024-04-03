199a72f785309425a2cb6bf8a5b07186c16360f0
Thomás Dixini <thomasdixini@gmail.com>
2024-04-03 19:39:39 -0300

    :recycle: refactor: Refatorando classe DAO, tirando query da classe principal e colocando na DAO

--- src/main/java/thomasdixini/App.java
+++ src/main/java/thomasdixini/App.java
@@ -17,21 +17,12 @@ public class App {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "1234");
 
-            String queryString = "CREATE TABLE " + "CLIENTE" +
-                    " (ID INT, NAME VARCHAR(255)," +
-                    " USERNAME CHAR(20), PASSWORD CHAR(20), CREATED_AT DATETIME, LAST_MODIFIED_AT DATETIME," +
-                    " PRIMARY KEY (ID));";
-
-            DAO.createTable(con, "cliente", queryString);
-
-            String querySelect = "SELECT ID, NAME, USERNAME, PASSWORD, CREATED_AT, LAST_MODIFIED_AT FROM CLIENTE";
-            DAO.select(con, "cliente", querySelect);
-
-            String queryInsert = "SELECT * FROM CLIENTE";
             Date date = new Date(System.currentTimeMillis());
-            DAO.insert(con, "cliente", queryInsert, 1, "Wesley", "wesleyteste", "1234", date, date);
 
-            DAO.update(con, "cliente", "");
+            DAO.createTable(con, "cliente");
+            DAO.select(con, "cliente");
+            DAO.insert(con, "cliente", 1, "Wesley", "wesleyteste", "1234", date, date);
+            DAO.update(con, "cliente");
         } catch (ClassNotFoundException e) {
             System.out.println(e);
         } catch (SQLException err) {
--- src/main/java/thomasdixini/jdbcdao/DAO.java
+++ src/main/java/thomasdixini/jdbcdao/DAO.java
@@ -2,55 +2,61 @@ package thomasdixini.jdbcdao;
 
 import java.sql.Connection;
 import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 public class DAO {
-    public static void createTable(Connection con, String table, String query) throws SQLException {
+    public static void createTable(Connection con, String table) throws SQLException {
+        String queryString = "CREATE TABLE " + table +
+                " (ID INT, NAME VARCHAR(255)," +
+                " USERNAME CHAR(20), PASSWORD CHAR(20), CREATED_AT DATETIME, LAST_MODIFIED_AT DATETIME," +
+                " PRIMARY KEY (ID));";
         try {
             Statement stmt = con.createStatement();
-            stmt.executeUpdate(query);
-        } catch(SQLException err) {
+            stmt.executeUpdate(queryString);
+        } catch (SQLException err) {
             System.out.println(
-                err.getSQLState() + "\n" +
-                err.getErrorCode() + "\n" +
-                err.getMessage() + "\n" +
-                err.getCause()
-                );
+                    err.getSQLState() + "\n" +
+                            err.getErrorCode() + "\n" +
+                            err.getMessage() + "\n" +
+                            err.getCause());
         }
     }
-    public static void select(Connection con, String table, String query) throws SQLException {
+
+    public static void select(Connection con, String table) throws SQLException {
+        String querySelect = "SELECT ID, NAME, USERNAME, PASSWORD, CREATED_AT, LAST_MODIFIED_AT FROM " + table;
+
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while(rs.next()) {
+            ResultSet rs = stmt.executeQuery(querySelect);
+            while (rs.next()) {
                 int id = rs.getInt("ID");
                 String name = rs.getString("NAME");
                 String username = rs.getString("USERNAME");
                 String password = rs.getString("PASSWORD");
                 Date created_at = rs.getDate("CREATED_AT");
-                Date last_modified_at = rs.getDate("LAST_MODIFIED_AT"); 
+                Date last_modified_at = rs.getDate("LAST_MODIFIED_AT");
                 System.out.println(id + name + username + password + created_at + last_modified_at);
             }
-        } catch(SQLException err) {
+        } catch (SQLException err) {
             System.out.println(
-                err.getSQLState() + "\n" +
-                err.getErrorCode() + "\n" +
-                err.getMessage() + "\n" +
-                err.getCause()
-                );
+                    err.getSQLState() + "\n" +
+                            err.getErrorCode() + "\n" +
+                            err.getMessage() + "\n" +
+                            err.getCause());
         }
     }
-    public static void insert(Connection con, String table, String query, int id, String name, String username, String password, Date created_at, Date last_modified_at) throws SQLException {
+
+    public static void insert(Connection con, String table, int id, String name, String username,
+            String password, Date created_at, Date last_modified_at) throws SQLException {
+
+        String queryInsert = "SELECT * FROM " + table;
+
         try {
             Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ResultSet rs = stmt.executeQuery(query);
+            ResultSet rs = stmt.executeQuery(queryInsert);
             rs.moveToInsertRow();
             rs.updateInt("ID", id);
             rs.updateString("NAME", name);
@@ -59,28 +65,27 @@ public class DAO {
             rs.updateDate("CREATED_AT", created_at);
             rs.updateDate("LAST_MODIFIED_AT", last_modified_at);
             rs.insertRow();
-        } catch(SQLException err) {
+        } catch (SQLException err) {
             System.out.println(
-                err.getSQLState() + "\n" +
-                err.getErrorCode() + "\n" +
-                err.getMessage() + "\n" +
-                err.getCause()
-                );
+                    err.getSQLState() + "\n" +
+                            err.getErrorCode() + "\n" +
+                            err.getMessage() + "\n" +
+                            err.getCause());
         }
     }
-    public static void update(Connection con, String table, String query) throws SQLException {
+
+    public static void update(Connection con, String table) throws SQLException {
         try {
-            PreparedStatement stmt = con.prepareStatement("UPDATE CLIENTE SET USERNAME = ? WHERE NAME = ?");
+            PreparedStatement stmt = con.prepareStatement("UPDATE " + table + " SET USERNAME = ? WHERE NAME = ?");
             stmt.setString(1, "thomas");
             stmt.setString(2, "Wesley");
             stmt.executeUpdate();
-        } catch(SQLException err) {
+        } catch (SQLException err) {
             System.out.println(
-                err.getSQLState() + "\n" +
-                err.getErrorCode() + "\n" +
-                err.getMessage() + "\n" +
-                err.getCause()
-                );
+                    err.getSQLState() + "\n" +
+                            err.getErrorCode() + "\n" +
+                            err.getMessage() + "\n" +
+                            err.getCause());
         }
     }
 }
